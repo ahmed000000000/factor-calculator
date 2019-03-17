a9f3ba1d3238aa3c9dad74a6835813e91b9b95db
ahmed000000000 <48419393+ahmed000000000@users.noreply.github.com>
2019-03-17 15:49:46 -0600

    Update factors.py

--- factors.py
+++ factors.py
@@ -5,6 +5,7 @@ factors=[]
 for i in range(1,num+1):
     if num%i == 0:
         factors.append(i)
+print(num, "has", len(factors), "factors")
 print("the factors of", num, "are",  factors)
 print(" ")
 
