d35d70bd69f447e3302843d2b9d975e7d64a010c
ahmed000000000 <48419393+ahmed000000000@users.noreply.github.com>
2019-03-18 00:57:59 -0600

    Update factors.py

--- factors.py
+++ factors.py
@@ -1,19 +1,28 @@
 #factor calculator
 num = int(input("Enter a number: \n"))
 print(" ")
+
+
 factors=[]
 for i in range(1,num+1):
     if num%i == 0:
         factors.append(i)
-print(num, "has", len(factors), "factors")
-print("the factors of", num, "are",  factors)
-print(" ")
 
-a = 0
-for i in factors:
-    if a < len(factors):
-        print(num, "divided by", factors[a], "is equal to ", num//factors[a])
-        a += 1
+
+if len(factors) ==2:
+    print(num, "is a prime number. Therefore the only factors are itself and 1")
+else:
+    print(num, "has", len(factors), "factors")
+    print("the factors of", num, "are",  factors)
+    print(" ")
+
+    a = 0
+    for i in factors:
+        if a < len(factors):
+            print(num, "divided by", factors[a], "is equal to ", num//factors[a])
+            a += 1
+
+
 
 print(" ")
 exit = input("AHMED'S FACTOR CALCULATOR")
